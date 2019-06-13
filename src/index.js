c20a7988ed12f8142e417b16a19419f75c2d5731
Mohit Karekar <karekar.mohit@gmail.com>
2019-06-13 09:29:15 +0530

    Updated to capture className

--- src/index.js
+++ src/index.js
@@ -5,16 +5,21 @@ function camelCase(s, delimiter) {
     return delimiter + a[1].slice(0, 1).toUpperCase() + a[1].slice(1, a[1].length)
 }
 
-function getEvents(el) {
-    let events = {}
+function getProps(el) {
+    let props = {}
 
-    for (k in el) {
+    let events = {}
+    for (let k in el) {
         if (/^on/.test(k) && el[k]) {
             events[camelCase(k, 'on')] = el[k]
         }
-    }
 
-    return events
+        if(k === 'className') {
+            props[k] = el[k]
+        }
+    }
+    props = Object.assign(props, events)
+    return props
 }
 
 function getChildren(elements) {
@@ -22,12 +27,10 @@ function getChildren(elements) {
     if (!elements) return children
     for (let i = 0; i < elements.length; i++) {
         let el = elements[i]
+        let props = { ...getProps(el) }
         if (el.childElementCount) {
-            let props = { ...getEvents(el) }
             children.push(React.createElement(el.tagName.toLowerCase(), props === {} ? null : props, ...getChildren(el.children)))
-        } else {
-            let props = { ...getEvents(el) }
-
+        } else if (el.textContent) {
             children.push(React.createElement(el.tagName.toLowerCase(), props === {} ? null : props, el.textContent))
         }
     }
@@ -35,7 +38,7 @@ function getChildren(elements) {
     return children
 }
 
-module.exports.convert = function(domElement) {
-    let props = { ...getEvents(domElement) }
+export const convert = function(domElement) {
+    let props = { ...getProps(domElement) }
     return React.createElement(domElement.tagName.toLowerCase(), props === {} ? null : props, ...getChildren(domElement.children))
 }
